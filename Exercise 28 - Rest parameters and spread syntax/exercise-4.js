2d0deb566db38c1a0ed61ed6ca78df75ed6147ca
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-03-22 19:50:13 +0100

    fixed

--- Exercise 28 - Rest parameters and spread syntax/exercise-4.js
+++ Exercise 28 - Rest parameters and spread syntax/exercise-4.js
@@ -1,6 +1,6 @@
-function sum(...item) {
-  return item.reduce((val1, val2) => val1+val2,0);
+function sum(num1, num2, num3) {
+  return num1 + num2 + num3;
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(...numbers));
