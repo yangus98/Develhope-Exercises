0b6baaaabf0c205143ffbf55297907d6c3573c60
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-03-27 16:44:48 +0200

    es 33

---
+++ Exercise 33 - Template Strings/exercise.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+const developer = new Person("Mario", "Rossi");
+console.log(`His name is ${developer.firstName} and his surname is ${developer.lastName}`);
+
+let code = `class Person {
+  constructor(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  }
+  }
+  
+  const developer = new Person("Mario", "Rossi");
+  console.log(developer.firstName + " " + developer.lastName);
+  `
+
+console.log(code);
