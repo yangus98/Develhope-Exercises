4f0a61cea245ff985508ed0c739826c182fae825
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-03-28 14:31:47 +0200

    more content es 30

--- Exercise 30 - JSON Methods/exercise-2.js
+++ Exercise 30 - JSON Methods/exercise-2.js
@@ -6,12 +6,14 @@ class Person {
     this.age = age;
   }
 
+  // il metodo è collegato, all'istanza newPerson e, attraverso this, alla classe Person
+
   toJson() {
     return JSON.stringify(this);
   }
 
   // il metodo, poichè static, è legato alla classe e non ad un'istanza. Si può richiamare solo con Person quindi.
-  // Static sui metodi non userà i campi di Person, altri valori a sè stanti.
+  // Static sui metodi non userà i campi di Person, ma altri valori a sè stanti.
 
   static fromJson(value) {
     return JSON.parse(value);
@@ -22,5 +24,10 @@ class Person {
 const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
 const developer = Person.fromJson(json);
 console.log(developer);
+
+const newPerson = new Person(2, "Giovanni", "Gargiulo", 30);
+
+console.log(newPerson);
+console.log(newPerson.toJson())
 
 
