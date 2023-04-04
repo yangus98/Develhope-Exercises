1cb603fd46407ddbb943b68105aa9eca28edb32e
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-04 09:48:48 +0200

    es 39

---
+++ Exercise 39 - Promise Errors/exercise.js
@@ -0,0 +1,28 @@
+const isLogged = true;
+
+function randomNumber(isLogged){
+    return new Promise((resolve,reject)=> {
+        if (isLogged == true) {
+            rand = Math.random();
+            resolve (rand);
+        } else {
+            reject (new Error("Button is turned OFF!!!"))
+        }
+    })
+}
+
+function whoAreU(num){
+    return new Promise((resolve,reject)=> {
+        if (num > 0.5) {
+            resolve (`{name: "John", age: 24}`)
+        } else {
+            reject (new Error("Random number is under 0.5"))
+        }
+    })
+}
+
+randomNumber(isLogged)
+.then (response =>{console.log(whoAreU(response))})
+.catch (e =>{console.log(e)})
+.finally (()=>{console.log()})
+
