a894cc724f2ac411a24496b241261526652f0e65
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-05 18:50:40 +0200

    es 34 fix 2

--- Exercise 34 - Callbacks/exercise.js
+++ Exercise 34 - Callbacks/exercise.js
@@ -1,18 +1,16 @@
-//function printAsyncName(name, sayHello){
-//    function sayHello(){
-//        return console.log("Hello")
-//   }
-    
-//    setTimeout(sayHello, 1000);
-//    return setTimeout(() =>{console.log(name)}, 2000);
-    
-//    }
-    
-//   printAsyncName("Luca");  
-
-function printAsyncName(name, callback){
+function printAsyncName(name, message){
+    let callback = () => console.log(message);
     setTimeout(callback, 1000);
     return setTimeout(() =>{console.log(name)}, 2000);
 }
 
-printAsyncName("Luca",() => console.log("Hello"))
+printAsyncName("Luca","Hello");
+
+//oppure
+
+//function printAsyncName(name, callback){
+//    setTimeout(callback, 1000);
+//    return setTimeout(() =>{console.log(name)}, 2000);
+//}
+
+//printAsyncName("Luca",() => console.log("Hello"))
