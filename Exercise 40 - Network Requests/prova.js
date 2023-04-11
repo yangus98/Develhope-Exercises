2817ea6393689708db5d18fc14bca22350e9a642
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-11 11:00:25 +0200

    es 40

---
+++ Exercise 40 - Network Requests/prova.js
@@ -0,0 +1,13 @@
+async function Fetcher(){
+const result = await fetch("https://jsonplaceholder.typicode.com/todos");
+const data = await result.json();
+  
+data.forEach(item => {
+  const todoList = document.createElement("li");
+  todoList.innerText = item.title;
+  document.querySelector("ul").appendChild(todoList);                   
+});
+console.log(data);
+};
+
+Fetcher();
