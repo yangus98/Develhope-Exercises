665735b3ce68939703b1427aacb2fdd7f0806fb4
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-13 19:33:48 +0200

    es 47

---
+++ Exercise 47 - DOM/exercise.js
@@ -0,0 +1,12 @@
+const nameText = document.querySelectorAll(".form-input")[0];
+const surnameText = document.querySelectorAll(".form-input")[1];
+const ageText = document.querySelectorAll(".form-input")[2]
+const nameLabel = document.querySelectorAll("label")[0];
+const surnameLabel = document.querySelectorAll("label")[1];
+const ageLabel = document.querySelectorAll("label")[2];
+
+console.log(`${nameLabel.innerText}${nameText.value}
+${surnameLabel.innerText}${surnameText.value}
+${ageLabel.innerText}${ageText.value}`
+);
+
