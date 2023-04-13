1fedf65d6af8d2a30ad392ac6f5ab790a6a22231
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-13 20:11:36 +0200

    es 46 with better scope

--- Exercise 46 - DOM/exercise.js
+++ Exercise 46 - DOM/exercise.js
@@ -2,3 +2,10 @@ const nameText = document.querySelectorAll(".form-input")[0];
 const surnameText = document.querySelectorAll(".form-input")[1];
 
 console.log(nameText.value + " " + surnameText.value);
+
+//oppure se si vuole puntare direttamente a tutti gli input type="text" della classe form-input
+
+const moreNameText = document.querySelectorAll("input[type=text].form-input")[0];
+const moreSurnameText = document.querySelectorAll("input[type=text].form-input")[1];
+
+console.log(moreNameText.value+ " " + moreSurnameText.value)
