c8d7430b90a1487f4e7eaae88838f52348025ef5
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-13 18:52:49 +0200

    es 46

---
+++ Exercise 46 - DOM/exercise.js
@@ -0,0 +1,4 @@
+const nameText = document.querySelectorAll(".form-input")[0];
+const surnameText = document.querySelectorAll(".form-input")[1];
+
+console.log(nameText.value + " " + surnameText.value);
