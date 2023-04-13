408941cc9cdd56cd48860927459a402aa5325e7b
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-13 12:29:10 +0200

    es 42

--- Exercise 42 - Network Requests/prova.js
+++ Exercise 42 - Network Requests/prova.js
@@ -1,10 +1,25 @@
-fetch("https://jsonplaceholder.typicode.com/posts",{
-method: "POST",
-body: {
-    "title":"application/json"
-    
-},
+const titolo = document.querySelector("#todo-title");
+const completo = document.querySelector("#todo-completed");
+const form = document.querySelector("form");
 
-}
-)
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const title = titolo.value;
+    const completed = completo.value;
+    obj = {
+        title : title,
+        completed: completed,
+    };
+     fetch("https://jsonplaceholder.typicode.com/posts", {
+        method : "POST",
+        headers : {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify(obj)
+     })
+     .then(response => response.json())
+     .then(data => console.log(data))
+     .catch(err => console.error(err))
+
+})
 
