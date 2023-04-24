278b0ea7fed102b2e5fbb4bf6724d3a690d25b29
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-24 19:56:18 +0200

    es 13

---
+++ conditional-rendering13-render-the-you-are-very-young/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Welcome from "./Welcome";
+let chooseName = "Mario";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome name ={chooseName}/>
+      </div>
+    );
+  }
+}
