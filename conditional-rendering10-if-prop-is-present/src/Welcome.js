5281ff1d0f1ef49bd81b64bb3fd1199149a28bba
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-24 19:17:55 +0200

    es 10

---
+++ conditional-rendering10-if-prop-is-present/src/Welcome.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Age from "./Age";
+Age.age = 18;
+
+export default class Welcome extends React.Component {
+  render() {
+    if (Age.age && true){
+      return (
+        <p>
+          Hello, {this.props.name}!
+          <Age age={Age.age} />
+        </p>
+      );
+    } else {
+      return (
+        <p>
+          Hello, {this.props.name}!
+        </p>
+      );
+    }
+  }
+}
+
+Welcome.defaultProps = {
+  name: "Undefined name!",
+  age: "Undefined age!"
+};
