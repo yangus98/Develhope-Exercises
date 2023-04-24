6bd9f74b751ed7b0211e12bb29e581313270452c
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-24 15:49:17 +0200

    es 5

---
+++ props5-default-value/src/Welcome.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <p>
+        Hello, {this.props.name}!
+      </p>
+    );
+  }
+}
+
+Welcome.defaultProps = {
+  name: "Default"
+}
+
+//if we don't set a default value to a prop and we don't declare it, the prop space will be empty.
