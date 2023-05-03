648ea70eaaa9338d9b8e7c8b71e4e081d5588065
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-05-03 15:17:22 +0200

    es 24

---
+++ 24 forms-automatic-focus/src/index.js
@@ -0,0 +1,5 @@
+import {createRoot} from "react-dom/client"
+import UncontrolledLogin from "./UncontrolledLogin"
+
+const root = createRoot(document.getElementById("root"));
+root.render(<UncontrolledLogin />);
