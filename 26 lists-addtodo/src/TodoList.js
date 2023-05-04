c493c9ce25a0df3f853cce086147dfc21dfc7b82
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-05-04 13:10:59 +0200

    es 26

---
+++ 26 lists-addtodo/src/TodoList.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+import {useRef} from "react"
+
+export default function TodoList() {
+
+  const input = useRef(null)
+  const [name, setName] = useState('');
+  const [items, setItems] = useState(["Strawberry Cake", "Profiterols", "Chocolate"]);
+  const handleAdd = () => {
+      setItems([...items, name]);
+      setName('');
+      input.current.value = ""
+    };
+
+  return (
+      <div>
+          <input onChange={(e) => setName(e.target.value)} ref={input}/>
+          <button onClick={handleAdd}>add</button>
+          {items.map((item) => {return <li>{item}</li>})}
+      </div>
+  );
+}
