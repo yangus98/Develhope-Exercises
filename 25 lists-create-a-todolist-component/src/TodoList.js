423356a54215a42623efe0edd24a370c6a56c809
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-05-04 12:49:12 +0200

    es 25 fixed

--- 25 lists-create-a-todolist-component/src/TodoList.js
+++ 25 lists-create-a-todolist-component/src/TodoList.js
@@ -1,28 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
-const sweets = ["Strawberry Cake", "Profiterols", "Ice Cream"]
-const items = sweets.map((sweet) => <li> {sweet} </li>)
-
-
-export default class TodoList extends React.Component {
-
-  addTodo = () => {
-    sweets.push("ciao")
-    console.log(sweets)
-  }
-
-  
-
-    render(){
-      return (
-        <>
-        <ul>
-        {items}
-        </ul>
-
-        <input type="text" />
-        <button onClick={this.addTodo}>click here</button>
-        </>
-      )
-    }
-  }
+export default function TodoList() {
+  const [name, setName] = useState('');
+  const [items, setItems] = useState(["Strawberry Cake", "Profiterols", "Chocolate"]);
+  const handleAdd = () => {
+      setItems([...items, name]);
+      setName('');
+  };
+  return (
+      <div>
+          <input onChange={(e) => setName(e.target.value)}/>
+          <button onClick={handleAdd}>add</button>
+          {items.map((item) => {return <li>{item}</li>})}
+      </div>
+  );
+}
