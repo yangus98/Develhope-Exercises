c244cfcbe636fa0efc7d9386f84c12c09617f679
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-05-07 10:35:08 +0200

    fix es 25

--- 25 lists-create-a-todolist-component/src/TodoList.js
+++ 25 lists-create-a-todolist-component/src/TodoList.js
@@ -5,13 +5,12 @@ export default function TodoList() {
   const [items, setItems] = useState(["Strawberry Cake", "Profiterols", "Chocolate"]);
   const handleAdd = () => {
       setItems([...items, name]);
-      setName('');
   };
   return (
       <div>
           <input onChange={(e) => setName(e.target.value)}/>
           <button onClick={handleAdd}>add</button>
-          {items.map((item) => {return <li>{item}</li>})}
+          {items.map((item, index) => {return <li index={index}>{item}</li>})}
       </div>
   );
 }
