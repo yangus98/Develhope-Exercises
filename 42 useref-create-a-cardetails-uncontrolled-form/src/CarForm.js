c1376c1e3a7dded0154f2d38240800d42f2ba2df
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-05-12 12:40:34 +0200

    es 42 fix

--- 42 useref-create-a-cardetails-uncontrolled-form/src/CarForm.js
+++ 42 useref-create-a-cardetails-uncontrolled-form/src/CarForm.js
@@ -1,6 +1,7 @@
 import { useRef } from "react"
 import { useState } from "react"
 
+
 export default function CarForm ({initialData}) {
 
     const carDetails = useRef("")
@@ -20,14 +21,18 @@ export default function CarForm ({initialData}) {
     formColor.value = color
 
     const onSubmit = (e) => {
-        e.preventDefault()
-        console.log([formName.value, formModel.value, formYear.value, formColor.value])
+      e.preventDefault()
+      console.log([formName.value, formModel.value, formYear.value, formColor.value])
+    }
+
+    const proveName = (e) => { 
+      setName(e.target.value)
     }
 
     return(
         <>
         <form ref={carDetails}>
-          <input ref={formName} type="text" onChange={e => setName(e.target.value)} defaultValue={name}/>
+          <input ref={formName} type="text" onChange={proveName} defaultValue={name} />
           <input ref={formModel} type="text" onChange={e => setModel(e.target.value)} defaultValue={model}/>
           <input ref={formYear} type="text" onChange={e => setYear(e.target.value)} defaultValue={year}/>
           <input ref={formColor} type="text" onChange={e => setColor(e.target.value)} defaultValue={color}/>
