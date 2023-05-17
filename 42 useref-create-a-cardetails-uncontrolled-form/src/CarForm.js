d6702e170420f17144d6e67a1c8c9ba80275fb8d
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-05-17 09:38:00 +0200

    es 42 fixed

--- 42 useref-create-a-cardetails-uncontrolled-form/src/CarForm.js
+++ 42 useref-create-a-cardetails-uncontrolled-form/src/CarForm.js
@@ -1,23 +1,23 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 
 export default function CarForm ({initialData}) {
 
-    const carDetails = useRef(null)
-    const formName = useRef(null)
-    const formModel = useRef(null)
-    const formYear = useRef(null)
-    const formColor = useRef(null)
+    const carDetails = useRef()
+    const formName = useRef()
+    const formModel = useRef()
+    const formYear = useRef()
+    const formColor = useRef()
 
     const onSubmit = (e) => {
       e.preventDefault()
       console.log([formName.current.value, formModel.current.value, formYear.current.value, formColor.current.value])
     }
 
-    
+    useEffect(() => {carDetails.current.reset();}, [initialData])
 
     return(
         <>
-        <form ref={carDetails} onFocus={e => (e.target.value = "")}>
+        <form ref={carDetails}>
           <input ref={formName} name="name" type="text" onChange={e => (e.target.value)} defaultValue={initialData.name}/>
           <input ref={formModel} name="model" type="text" onChange={e => (e.target.value)} defaultValue={initialData.model}/>
           <input ref={formYear} name="year" type="text" onChange={e => (e.target.value)} defaultValue={initialData.year}/>
