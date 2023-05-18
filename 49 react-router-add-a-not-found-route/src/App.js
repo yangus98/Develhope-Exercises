7945ebe55fa1c5dd3837254f5b4784a14a4a4fff
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-05-18 19:13:13 +0200

    es 49 fix

--- 49 react-router-add-a-not-found-route/src/App.js
+++ 49 react-router-add-a-not-found-route/src/App.js
@@ -3,14 +3,17 @@ import { Route } from "react-router-dom"
 import { Link } from "react-router-dom"
 import ShowGithubUser from "./ShowGithubUser"
 
+
 export function App(){
 
     return(
         <>
-        <Routes>
+        <Routes> 
             <Route path="/" element={<ShowGithubUser />} />
-            <Route path="users/:username" element={<ShowGithubUser />} />       
+            <Route path="users/:username" element={<ShowGithubUser />} />
+            <Route path="*" element={<h1>Page not found!</h1>} />   
         </Routes>
+
         <p><Link to="users/yangus98">Link to my repo...</Link></p>
         <p><Link to="users/rushizo">Link to my friend Danilo...</Link></p>
         <p><Link to="users/Lucone00">Link to my friend Luca...</Link></p>
--- 49 react-router-add-a-not-found-route/src/ShowGithubUser.js
+++ 49 react-router-add-a-not-found-route/src/ShowGithubUser.js
@@ -2,7 +2,7 @@ import GithubUser from "./GithubUser"
 import { useParams } from "react-router-dom"
 
 export default function ShowGithubUser(){
-    const {username = "INVALID-NAME"} = useParams()
+    const {username} = useParams()
 
     return(
         <>
