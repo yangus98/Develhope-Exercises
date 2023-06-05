ac433fce76465e0fe1b173b08c0ee2ce3706db8a
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-06-05 21:10:46 +0200

    es 9

---
+++ 9.mjs
@@ -0,0 +1,29 @@
+async function checkPlayer(player){
+  try{
+    const letsPlay = await new Promise(function (resolve, reject) {
+    if(player === "Joe" || player === "Caroline" || player === "Sabrina") {
+      async function luckyDraw(player) {
+        return await new Promise((resolve, reject) => {
+          const win = Boolean(Math.round(Math.random()));
+      
+          process.nextTick ( () => {
+            if (win) {
+              resolve (console.log(`${player} won a prize in the draw!`));
+            }
+
+            reject(new Error(`${player} lost the draw.`));
+          
+          });
+        });
+      }
+    return resolve(luckyDraw(player))
+    } else {
+      return reject("Player not allowed!");
+}
+})
+} catch(e) {
+console.error(e)
+}
+}
+
+checkPlayer("Caroline")
