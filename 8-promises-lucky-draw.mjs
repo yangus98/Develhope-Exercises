996971afc8efa529438e6d892ba1a2450cca8063
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-06-06 08:42:29 +0200

    es 8-9 fixed

--- 8-promises-lucky-draw.mjs
+++ 8-promises-lucky-draw.mjs
@@ -1,25 +1,33 @@
-function checkPlayer(player) {
-const letsPlay = new Promise(function (resolve, reject) {
-  if(player === "Joe" || player === "Caroline" || player === "Sabrina") {
-    function luckyDraw(player) {
-      return new Promise((resolve, reject) => {
-        const win = Boolean(Math.round(Math.random()));
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
     
-        process.nextTick(() => {
-          if (win) {
-            resolve(`${player} won a prize in the draw!`);
-          } else {
-            reject(new Error(`${player} lost the draw.`));
-          }
-        });
-      });
-    }
-  return resolve(luckyDraw(player))
-} else {
-  return reject(new Error("player not allowed!"))
-}
-}).then(data => console.log(data)).catch(err => console.log(err))
+      const win = Boolean(Math.round(Math.random()));
 
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(`${player} lost the draw.`);
+        }
+  });
+ })
 }
 
-checkPlayer("Caroline")
+Promise.allSettled([
+  luckyDraw("Joe"),
+  luckyDraw("Caroline"),
+  luckyDraw("Sabrina")
+])
+  .then((results) => {
+    
+    results.forEach((result) => {
+      if (result.status === "fulfilled") {
+        console.log(result.value);
+      } else {
+        console.log(result.reason);
+      }
+    });
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
--- 9 await-the-lucky-draw-results.mjs
+++ 9 await-the-lucky-draw-results.mjs
@@ -1,29 +1,37 @@
-async function getResults(player){
+async function getResults(){
   try{
-    const letsPlay = await new Promise(function (resolve, reject) {
-    if(player === "Joe" || player === "Caroline" || player === "Sabrina") {
-      async function luckyDraw(player) {
-        return await new Promise((resolve, reject) => {
-          const win = Boolean(Math.round(Math.random()));
-      
-          process.nextTick ( () => {
-            if (win) {
-              resolve (console.log(`${player} won a prize in the draw!`));
-            }
-
-            reject(new Error(`${player} lost the draw.`));
-          
-          });
-        });
+    async function luckyDraw(player) {
+        const win = Boolean(Math.round(Math.random()));
+        if (win) {
+          return `${player} won a prize in the draw!`;
+        } else {
+          throw `${player} lost the draw.`;
+        }
       }
-    return resolve(luckyDraw(player))
-    } else {
-      return reject("Player not allowed!");
-}
-})
-} catch(e) {
-console.error(e)
-}
+
+    async function runLuckyDraw() {
+      try {
+        const results = await Promise.allSettled([
+          luckyDraw("Tina"),
+          luckyDraw("Jorge"),
+          luckyDraw("Julien"),
+        ]);
+        results.forEach((result) => {
+          if (result.status === "fulfilled") {
+            console.log(result.value);
+          } else {
+            console.log(result.reason);
+          }
+        });
+      } catch (error) {
+        console.log(`Error: ${error}`);
+      }
+    }
+    
+    runLuckyDraw()
+  }catch(error){
+    throw error;
+  }
 }
 
-getResults("Caroline")
+getResults()
