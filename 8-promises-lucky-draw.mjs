cb43babb08ebd6658ef3ba90a0661b684a9dea37
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-06-07 12:51:30 +0200

    fix es 8

--- 8-promises-lucky-draw.mjs
+++ 8-promises-lucky-draw.mjs
@@ -13,21 +13,32 @@ function luckyDraw(player) {
  })
 }
 
-Promise.allSettled([
-  luckyDraw("Joe"),
-  luckyDraw("Caroline"),
-  luckyDraw("Sabrina")
-])
-  .then((results) => {
+luckyDraw('Joe')
+.then(data => console.log(data))
+.catch(error => console.log(error))
+.then(() => luckyDraw('Caroline'))
+.then(data => console.log(data))
+.catch(error => console.log(error))
+.then(() => luckyDraw('Marco'))
+.then(data => console.log(data))
+.catch(error => console.log(error))
+
+//Promise.allSettled([
+//  luckyDraw("Joe"),
+//  luckyDraw("Caroline"),
+//  luckyDraw("Sabrina")
+//])
+//  .then((results) => {
     
-    results.forEach((result) => {
-      if (result.status === "fulfilled") {
-        console.log(result.value);
-      } else {
-        console.log(result.reason);
-      }
-    });
-  })
-  .catch((error) => {
-    console.log(`Error: ${error}`);
-  });
+//    results.forEach((result) => {
+//      if (result.status === "fulfilled") {
+//        console.log(result.value);
+//      } else {
+//        console.log(result.reason);
+//      }
+//    });
+//  })
+//  .catch((error) => {
+//    console.log(`Error: ${error}`);
+//  });
+
