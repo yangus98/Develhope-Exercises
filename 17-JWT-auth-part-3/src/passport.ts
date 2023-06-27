8461a0c3f0feb5854fd7dcbf358435cf4a7d48e8
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-06-27 08:20:21 +0200

    es 15-16-17

--- 17-JWT-auth-part-3/src/passport.ts
+++ 17-JWT-auth-part-3/src/passport.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import passport from "passport"
 import passportJWT from "passport-jwt"
-import { db } from "./db"
+import { db } from "./db.js"
 
 const { SECRET } = process.env;
 
@@ -13,7 +13,6 @@ passport.use(
     }, 
     async (payload, done) => {
         const user = await db.one(`SELECT * FROM users WHERE id=$1`, payload.id)
-        console.log(user);
 
         try{
             return user ? done(null, user) : done(new Error("User not found."))
--- 17-JWT-auth-part-3/src/server.ts
+++ 17-JWT-auth-part-3/src/server.ts
@@ -3,8 +3,10 @@ import "express-async-errors"
 import morgan from "morgan"
 import 'dotenv/config.js'
 import { getAll, getOneById, create, updateById, deleteById, createImage } from "./controllers/planets.js";
-import {logIn, signUp} from "./controllers/users.js";
+import {logIn, signUp, logOut} from "./controllers/users.js";
+import authorize from "./authorize.js"
 import multer from "multer";
+import "./passport.js"
 
 const storage = multer.diskStorage({
   destination:(req, file, cb) => {
@@ -56,6 +58,7 @@ app.post("/api/planets/:id/image", upload.single("image"), createImage)
 
 app.post("/api/users/login", logIn )
 app.post("/api/users/signup", signUp )
+app.get("/api/users/logout", authorize, logOut )
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
